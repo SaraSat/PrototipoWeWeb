f7ec4576a58663d69e0f9a21a9eb128e650737ee
Sara Álvarez Turrión <sara_sat93@hotmail.com>
2020-10-21 10:21:57 +0200

    prototipado hecho

--- src/app/central/central.component.ts
+++ src/app/central/central.component.ts
@@ -8,7 +8,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CentralComponent implements OnInit {
 
-  titulo: string = "WebWe"
+  titulo: string = "SkyWeb"
 
   constructor() { }
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  titulo:string ="WebWe"
+  titulo:string ="SkyWeb"
 
   constructor() { }
 
